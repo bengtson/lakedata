32c2b17965e873638392d213b25465b36ebff829
Michael Bengtson <michael.bengtson@gmail.com>
2016-04-10 07:59:18 -0500

    Added Documentation

--- lib/lakedata.ex
+++ lib/lakedata.ex
@@ -7,10 +7,12 @@ defmodule LakeData do
 
 	Given a dock measurement and a water temperature measurement, the parameters for sending to the Lake Sara website are generated.
 
-	The calculation for the water level ASL is: 581.72-(dock+3.0)/12.0
+	The calculation for the water level ASL is: 581.72-(seawall+3.0)/12.0
+
+	This equation is based on the survey height of the top of the doc at 581.72 feet above sealevel. The dock is 3" higher than the seawall. Given the seawall height above the water in inches, + 3 inches to the top of the dock, then converted to feet and subtracted from the feet above sealevel gives the current level of the water.
 	"""
-	def water_level_from_dock (dock) do
-		waterlevel = 581.72-(dock+3.0)/12.0
+	def water_level_from_seawall (seawall) do
+		581.72-(seawall+3.0)/12.0
 #		IO.puts "Lake Level: " <> to_string(waterlevel)
 	end
 end
